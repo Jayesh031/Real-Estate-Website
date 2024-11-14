0e742f7c94870c8621e2676493615338d45d2158
Jayesh <jayeshambekar31@gmail.com>
2024-11-14 17:28:38 +0530

    correct spelling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -42,7 +42,7 @@ export default function Contact(){
             whileInView={{opacity:1,x:0}}
             viewport={{once:true}}
             className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden" id="Contact">
-            <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">Customer <span className="underline underline-offset-4 decoration-1 under font-light">With Us</span></h1>
+            <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">Contact <span className="underline underline-offset-4 decoration-1 under font-light">With Us</span></h1>
             <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">Ready to Make a Move? Lets's Build Your Future Together</p>
 
 
